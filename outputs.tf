1a0427d8c63d18e470a05144b21da34786999274
Malik Weathers <malikweathers17@yahoo.com>
2025-11-20 21:47:36 -0500

    Completed Day 6 - Remote state backend with S3/DynamoDB and modular architecture

--- outputs.tf
+++ outputs.tf
@@ -22,3 +22,14 @@ output "api_endpoint" {
   description = "API Gateway endpoint URL"
   value       ="${aws_api_gateway_stage.api_stage.invoke_url}/items"
 }
+
+# Output the bucket name 
+output "state_bucket_name" {
+  description = "Name of the S3 bucket"
+  value = aws_s3_bucket.terraform_state.id
+}
+
+output "lock_table_name" {
+  description = "Name of the DynamoDB table for state locking"
+  value = aws_dynamodb_table.terraform_state_lock.id
+}
